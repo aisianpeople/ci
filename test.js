7b4423dfc35493933a53210d375b70a6ca554193
Scott Mottola <sjmottola@gmail.com>
2021-07-19 13:54:11 -0600

    update test

--- test.js
+++ test.js
@@ -6,6 +6,10 @@ test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
 
+test('can create a basic greeting', () => {
+  expect(getGreeting('Franky')).toBe("Hi Franky");
+});
+
 test('returns a number', () => {
   expect(getNumber()).toBe(2);
 });
